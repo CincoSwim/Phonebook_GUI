4a6d8c46198b1bd0c3bbf1cdb8e3d9a9ae403f49
CincoSwim <cgpucko@gmail.com>
2018-04-25 21:40:17 -0400

    Implements a file reader, to read phonebook.txt in the root to an Entry array. This array is then used to fill a TableView. TODO- Implement Buttons, Text fields, Merge functionality, Search functionality, and file writing. Remember that strings need to be formatted to phone number format before being put into the Entry object!

--- src/sample/Entry.java
+++ src/sample/Entry.java
@@ -1,13 +1,46 @@
 package sample;
 
 public class Entry {
-    String name, notes;
-    long number;
+    public String name, notes;
+    public String number;
 
-    public Entry(String nameEntry, long numberEntry, String notesEntry) {
-        name = nameEntry;
-        number = numberEntry;
-        notes = notesEntry;
+    public Entry(){
+        this.name = "";
+        this.number = "";
+        this.notes = "";
+    }
+
+    public Entry(String nameEntry, String numberEntry, String notesEntry) {
+        this.name = nameEntry;
+        this.number = numberEntry;
+        this.notes = notesEntry;
+
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 }
 
--- src/sample/Phonebook.java
+++ src/sample/Phonebook.java
@@ -2,9 +2,12 @@ package sample;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.application.Application;
@@ -14,6 +17,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -21,56 +27,131 @@ import javax.swing.*;
 
 
 public class Phonebook extends Application {
-    Button button;
     Stage window;
-    Scene scene1, scene2;
+    TableView<Entry> table;
+    static int index;
+    static Entry EntryList[] = new Entry[200];
+    static File phonebook;
+    public static void main(String[] args) throws FileNotFoundException {
 
+        //These lines just set some dummy entries.
+        //This'll get replaced with a method to read/write from a text document, just like before.
+        EntryList[0] = new Entry ("Chris Pucko", "513-683-7895", "Cool dudee");
+        EntryList[1] = new Entry ("Jonathan DOKOKOKOKOKO", "phonenumberhereplacehlder", "Meta, bro.");
+        index = 3;
+        //End test entries
+        ReadsPhoneBook();
+        getsEntries(EntryList, index);
+        System.out.println(index);
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("thenewboston");
 
-        HBox topmenu = new HBox();
-        Button buttonA = new Button("file");
-        Button buttonB = new Button("edit");
-        Button buttonC = new Button("view");
+        //Name Column
+        TableColumn<Entry, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        topmenu.getChildren().addAll(buttonA, buttonB, buttonC);
+        //Number Column
+        TableColumn<Entry, String> numColumn = new TableColumn<>("Number");
+        numColumn.setMinWidth(200);
+        numColumn.setCellValueFactory(new PropertyValueFactory<>("number"));
 
-        VBox leftmenu = new VBox();
-        Button buttonD = new Button("Dee");
-        Button buttonE = new Button("Ee");
-        Button buttonF = new Button("Eph");
+        //Notes Column
+        TableColumn<Entry, String> notesColumn = new TableColumn<>("Notes");
+        notesColumn.setMinWidth(300);
+        notesColumn.setCellValueFactory(new PropertyValueFactory<>("notes"));
 
-        leftmenu.getChildren().addAll(buttonD, buttonE, buttonF);
-
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(topmenu);
-        borderPane.setLeft(leftmenu);
+        table = new TableView<>();
+        table.setItems(getsEntries(EntryList, index));
+        table.getColumns().addAll(nameColumn, numColumn, notesColumn);
 
 
-        Scene scene = new Scene(borderPane, 300, 200);
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(table);
+
+        Scene scene = new Scene(vBox);
+
+
         window.setScene(scene);
         window.show();
 
 
     }
-    private void closeProgram(){
-        Boolean answer = Confirmbox.display("title", "Sure you want to exit?");
-        if (answer){
-            System.out.println("File is saved");
-            window.close();}
+    public static int ReadsPhoneBook() throws FileNotFoundException {
+        //Static Method that reads entries from included phonebook.txt.
+        //As each entry is read, a new Entry object is created.
+
+        try {
+            phonebook = new File("phonebook.txt");
+            if (!phonebook.exists())
+                phonebook.createNewFile();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        Scanner read = new Scanner(phonebook);
+        String name, notes, number;
+
+
+        index = 0;
+
+        try {
+
+
+            for (int i = 0; i < 200; i++) {
+                name = read.nextLine();
+                number = read.nextLine();
+                notes = read.nextLine();
+                EntryList[i] = new Entry(name, number, notes);
+                index++;
+
+            }
+        } catch (Exception NoSuchElementException) {
+            System.out.println("Now Loaded!");
+            return index;
+        }
+        return index;
+    }
+    public static void WritesPhoneBook() throws FileNotFoundException {
+        //Method that writes each objects name, number and notes for easy storage.
+        //Formatted so that it can be read back by ReadsPhoneBook().
+        PrintStream P = new PrintStream("phonebook.txt");
+        for (int i = 0; i < index; i++) {
+            P.println(EntryList[i].name);
+            P.println(EntryList[i].number);
+            P.println(EntryList[i].notes);
+        }
+        P.close();
+        System.out.println("Phonebook Saved");
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    //Creates and Returns an observable list made of Entry objects.
+    public static ObservableList<Entry> getsEntries(Entry[] entryArray, int index){
+
+        ObservableList<Entry> entries = FXCollections.observableArrayList();
+
+        for (int i=0; i < index; i++)//make sure index is static through the class. Really bad if it goes out of bounds.
+            entries.add(entryArray[i]);
+
+        return entries;
+    }
+
+    private void closeProgram() {
+        Boolean answer = Confirmbox.display("title", "Sure you want to exit?");
+        if (answer) {
+            System.out.println("File is saved");
+            window.close();
+        }
+    }
+}
 
 
-        launch(args);
-
-
-        //Reimplement these with buttons somehow.
-        //need to display entries as well.
+//Reimplement these with buttons somehow.
+//need to display entries as well.
                 /*
                     case 'e':
                         nameEntry = commandEntry.substring(2);
@@ -93,18 +174,8 @@ public class Phonebook extends Application {
                         //if (!qSuccess)
                         //    System.out.println("** No entry found for " + query);
                         break;
-                    case 'l':
-                        System.out.println();
-                        //listsEntries();
-                        break;
 
-                    case 'q':
-                        quitCondition = 'q';
-                        System.out.println("Saving and Shutting Down......");
-                        //WritesPhoneBook();
-                        System.exit(0);
-*/
-    }
+
 
 }
 
@@ -112,4 +183,4 @@ public class Phonebook extends Application {
 
 
 
-
+*/
